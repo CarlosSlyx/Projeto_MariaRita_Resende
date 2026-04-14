238361b3e572af3713fede97d92ad82690dddd17
Maria Rita <mariaritast@gmail.com>
2026-04-14 16:00:22 -0300

    Criado o Metodo MA3()

--- A.java
+++ A.java
@@ -15,4 +15,7 @@ public class A {
     public void MA2() {
         System.out.println("Metodo MA2");
     }
+    public void MA3() {
+        System.out.println("Alteracao a classe A partir do clone");
+}
 }
